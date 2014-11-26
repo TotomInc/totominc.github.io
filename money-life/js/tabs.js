9b5f75e6d49b8760aefbcbd8f2e88b6e9b3b9ddd
BdrTotom <totom7700@gmail.com>
2014-11-26 19:48:13 +0100

    Money Life - Bug Fix

--- money-life/js/tabs.js
+++ money-life/js/tabs.js
@@ -1,1 +1,6 @@
-function tabclick(a){$("#content-"+a).attr("style","display: block"),$("#content-"+lastname).attr("style","display: none"),lastname=a}var lastname="1";tabclick(lastname);
+var anc_onglet = "1";
+tabclick(anc_onglet);
+function tabclick(a) {
+	document.getElementById("content-" + anc_onglet).style.display = "none",
+    document.getElementById("content-" + a).style.display = "block", anc_onglet = a
+}
