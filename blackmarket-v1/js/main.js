1f24c57e618cb646af3c6c5b31a0b2b5b452d378
BdrTotom <totom7700@gmail.com>
2014-12-07 11:20:29 +0100

    Blackmarket - Proto Up #2

--- blackmarket-v1/js/main.js
+++ blackmarket-v1/js/main.js
@@ -1,74 +1,66 @@
-var money;
-var ammo; var drug;
+var money; var ammo; var drug; var progress;
 
 var upgradesOwned;
 var upgrades = [
-    new Upgrade("Copper bullets : 1.50$/shoot", 30, function() {console.log(test)})
+    new Upgrade("Up 1.10 : shoot reward x2!",   30,     function() {ammo[1] *= 2}),
+    new Upgrade("Up 1.11 : shoot reward x2!",   100,    function() {ammo[1] *= 2}),
+    new Upgrade("Up 1.12 : shoot reward x2!",   500,    function() {ammo[1] *= 2}),
+    new Upgrade("Up 1.13 : shoot reward x2!",   3000,   function() {ammo[1] *= 2})
 ];
 
-var allVars = ['money','ammo','drug'];
+var helpersOwned;
+var helpers = [
+    new Helper("Shoot Helper",      50),
+    new Helper("Reload Helper",     1000),
+    new Helper("Cook Helper",       750),
+    new Helper("Sell Drug Helper",  3000)
+];
+
+var allVars = ['money','ammo','drug','progress','upgradesOwned','helpersOwned'];
 
 // Saving system
 function setItem(key, value) { localStorage.setItem(key, JSON.stringify(value)); };
 function getItem(key) { return JSON.parse(localStorage.getItem(key)); };
 function removeItem(key) { localStorage.removeItem(key); };
-function saveData() {
-    for (var i = 0; i < allVars.length; i++) {
-        setItem(allVars[i], window[allVars[i]]);
-    };
-};
+function saveData() { for (var i = 0; i < allVars.length; i++) { setItem(allVars[i], window[allVars[i]]); }; };
 function loadData() {
-    for (var i = 0; i < allVars.length; i++) {
-        if (getItem(allVars[i]) != null && getItem(allVars[i]) != undefined) {
-            window[allVars[i]] = getItem(allVars[i]);
-        };
-    };
-    updateStats();
-    updateActions();
+    for (var i = 0; i < allVars.length; i++) { if (getItem(allVars[i]) != null && getItem(allVars[i]) != undefined) { window[allVars[i]] = getItem(allVars[i]); }; };
+    updateStats(); updateActions();
 };
-function resetData() {
-    for (var i = 0; i < allVars.length; i++) {
-        removeItem(allVars[i]);
-    }; location.reload();
-};
-
-// Basic functions
-function shoot() {
-    if (ammo[0] >= 1) { ammo[0] -= 1; $("#shoot").attr('onclick', '');
-        setTimeout(function() { money += ammo[1]; $("#shoot").attr('onclick', 'shoot()'); updateStats(); }, ammo[2]);
-        $("#b-f1").animate({width: "100%"}, ammo[2], "linear");
-        $("#b-f1").animate({width: "0%"}, 10);
-    };
-};
-function reload() {
-    if (ammo[0] == 0) { $("#reload").attr('onclick', '');
-        setTimeout(function() { ammo[0] = 12; $("#reload").attr('onclick', 'reload()'); updateStats(); }, ammo[3]);
-        $("#b-f2").animate({width: "100%"}, ammo[3], "linear");
-        $("#b-f2").animate({width: "0%"}, 10);
-    };
-};
-function cook() {
-    $("#cook").attr('onclick', '');
-    setTimeout(function() { drug[0] += drug[1]; $("#cook").attr('onclick', 'cook()'); updateStats(); }, drug[3]);
-    $("#b-f3").animate({width: "100%"}, drug[3], "linear");
-    $("#b-f3").animate({width: "0%"}, 10);
-};
-function sell() {
-    if (drug[0] >= drug[5]) { $("#sell").attr('onclick', '');
-        setTimeout(function() { drug[0] -= drug[5]; money += drug[5] * drug[2]; $("#sell").attr('onclick', 'sell()'); updateStats(); }, drug[4]);
-        $("#b-f4").animate({width: "100%"}, drug[4], "linear");
-        $("#b-f4").animate({width: "0%"}, 10);
-    };
-};
+function resetData() { for (var i = 0; i < allVars.length; i++) { removeItem(allVars[i]); }; location.reload(); };
 
 // Helpers
 function initVars() {
     money = 0;
-    ammo = [12, 0.75, 1500, 5000]; // ammo stock - reward - time - reload time
-    drug = [0, 2, 4, 15000, 7500, 1]; // drug stock - prod - reward/g - prod time - sell time - g. sold/click
+    ammo = [12, 0.75, 1500, 5000];      // stock - reward   - time      - reload time
+    drug = [0, 2, 4, 15000, 7500, 1];   // stock - prod     - reward/g  - prod time     - sell time - g.sold/click
 
     upgradesOwned = [];
     for (var i = 0; i < upgrades.length; i++) { upgradesOwned.push(false); };
+
+    helpersOwned = [];
+    for (var i = 0; i < helpers.length; i++) { helpersOwned.push(false); };
+
+    progress = [];
+    for (var i = 0; i < helpers.length; i++) { progress.push(0); };
+};
+function initGame() {
+    updateStats();
+    updateActions();
+
+    for (var i = 0; i < upgrades.length; i++) {
+        var u = upgrades[i];
+        $("#u-n" + (i+1)).html(u.name);
+        $("#u-c" + (i+1)).html("<br> cost : " + fix(u.price, 0) + "$");
+        if (upgradesOwned[i]) { $("#u-" + (i+1)).css('display', 'none'); };
+    };
+
+    for (var i = 0; i < helpers.length; i++) {
+        var h = helpers[i];
+        $("#h-n" + (i+1)).html(h.name);
+        $("#h-c" + (i+1)).html(" - cost : " + h.price + "$");
+        if (helpersOwned[i]) { $("#h-" + (i+1)).css('display', 'none'); };
+    };
 };
 function updateStats() {
     $("#s-money").html("Money : " + fix(money, 2) + "$");
@@ -85,11 +77,83 @@ function updateActions() {
     $("#a-cook").html("+ " + fix(drug[1], 2) + "g/click - " + fix((drug[3]/1000), 2) + "s");
     $("#a-sell").html("+ " + fix((drug[5] * drug[2]), 2) + "$/click - " + fix((drug[4]/1000), 2) + "s");
 };
-function updateUpgrades() {
-    for (var i = 0; i < upgrades.length; i++) {
-        var u = upgrades[i];
-        $("#u-n" + (i+1)).html(u.name);
-        $("#u-c" + (i+1)).html(" - cost " + fix(u.price, 0) + "$");
+function updateGame() {
+    autoShoot();
+    autoReload();
+    autoCook();
+    autoSell();
+};
+
+// Basic functions
+function shoot() {
+    if (ammo[0] >= 1 && helpersOwned[0] == false) { ammo[0] -= 1; $("#shoot").attr('onclick', '');
+        setTimeout(function() { money += ammo[1]; $("#shoot").attr('onclick', 'shoot()'); updateStats(); }, ammo[2]);
+        $("#b-f1").animate({width: "100%"}, ammo[2], "linear");
+        $("#b-f1").animate({width: "0%"}, 10);
+    };
+};
+function autoShoot() {
+    if (helpersOwned[0] == true && ammo[0] >= 1) {
+        progress[0] += 0.01;
+        var width = progress[0]/(ammo[2]/1000) * 100;
+        $("#b-f1").css('width', width + '%');
+        if (progress[0] > (ammo[2]/1000)) {
+            progress[0] = 0; money += ammo[1]; ammo[0]--; updateStats();
+            $("#b-f1").css('width', 0);
+        };
+    };
+};
+function reload() {
+    if (ammo[0] == 0) { $("#reload").attr('onclick', '');
+        setTimeout(function() { ammo[0] = 12; $("#reload").attr('onclick', 'reload()'); updateStats(); }, ammo[3]);
+        $("#b-f2").animate({width: "100%"}, ammo[3], "linear");
+        $("#b-f2").animate({width: "0%"}, 10);
+    };
+};
+function autoReload() {
+    if (helpersOwned[1] == true && ammo[0] == 0) {
+        progress[1] += 0.01;
+        var width = progress[1]/(ammo[3]/1000) * 100;
+        $("#b-f2").css('width', width + '%');
+        if (progress[1] > (ammo[3]/1000)) {
+            progress[1] = 0; ammo[0] = 12; updateStats();
+            $("#b-f2").css('width', 0);
+        };
+    };
+};
+function cook() {
+    $("#cook").attr('onclick', '');
+    setTimeout(function() { drug[0] += drug[1]; $("#cook").attr('onclick', 'cook()'); updateStats(); }, drug[3]);
+    $("#b-f3").animate({width: "100%"}, drug[3], "linear");
+    $("#b-f3").animate({width: "0%"}, 10);
+};
+function autoCook() {
+    if (helpersOwned[2] == true) {
+        progress[2] += 0.01;
+        var width = progress[2]/(drug[3]/1000) * 100;
+        $("#b-f3").css('width', width + '%');
+        if (progress[2] > (drug[3]/1000)) {
+            progress[2] = 0; drug[0] += drug[1]; updateStats();
+            $("#b-f3").css('width', 0);
+        };
+    };
+};
+function sell() {
+    if (drug[0] >= drug[5]) { $("#sell").attr('onclick', '');
+        setTimeout(function() { drug[0] -= drug[5]; money += drug[5] * drug[2]; $("#sell").attr('onclick', 'sell()'); updateStats(); }, drug[4]);
+        $("#b-f4").animate({width: "100%"}, drug[4], "linear");
+        $("#b-f4").animate({width: "0%"}, 10);
+    };
+};
+function autoSell() {
+    if (helpersOwned[3] == true && drug[0] >= drug[5]) {
+        progress[3] += 0.01;
+        var width = progress[3]/(drug[4]/1000) * 100;
+        $("#b-f4").css('width', width + '%');
+        if (progress[3] > (drug[4]/1000)) {
+            progress[3] = 0; drug[0] -= drug[5]; money += drug[5] * drug[2]; updateStats();
+            $("#b-f4").css('width', 0);
+        };
     };
 };
 
@@ -102,15 +166,32 @@ function Upgrade(name, price, run) {
 function buyUpgrade(index) {
     if (money >= upgrades[index].price) {
         money -= upgrades[index].price;
+        upgradesOwned[index] = true;
         upgrades[index].run();
+        $("#u-" + (index+1)).css('display', 'none');
         updateStats(); updateActions();
     };
 };
 
+function Helper(name, price) {
+    this.name = name;
+    this.price = price;
+};
+function buyHelper(index) {
+    if (money >= helpers[index].price) {
+        money -= helpers[index].price;
+        helpersOwned[index] = true;
+        $("#h-" + (index+1)).css('display', 'none');
+        updateStats(); updateActions();
+    };
+};
+
+// Loop + onload
 window.onload = function() {
     initVars();
     loadData();
-    updateStats();
-    updateActions();
-    updateUpgrades();
+    initGame();
 };
+window.setInterval(function() {
+    updateGame();
+}, 10);
