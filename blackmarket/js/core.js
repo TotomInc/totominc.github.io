f00f306531789da259b750d60d7c90569292b44b
Tom <totom7700@gmail.com>
2015-01-13 19:19:52 +0100

    Blackmarket - Prototype v0.020r3

--- blackmarket/js/core.js
+++ blackmarket/js/core.js
@@ -7,28 +7,33 @@ var dInit = [
 ];
 var upgradesOwned;
 var upgrades = [
-    new Upgrade("Shoot reward x3", 12, function() { ammo[1] *= 3 }), // *7
-    new Upgrade("Shoot reward x3", 60, function() { ammo[1] *= 3 }),
-    new Upgrade("Shoot reward x2.5", 420, function() { ammo[1] *= 2.5 }),
-    new Upgrade("Shoot reward x2.5", 2940, function() { ammo[1] *= 2.5 }),
+    new Upgrade("Shoot reward x3", 12, function() { shoot[1] *= 3 }), // *7
+    new Upgrade("Shoot reward x3", 60, function() { shoot[1] *= 3 }),
+    new Upgrade("Shoot reward x2.5", 420, function() { shoot[1] *= 2.5 }),
+    new Upgrade("Shoot reward x2.5", 2940, function() { shoot[1] *= 2.5 }),
 
-    new Upgrade("Shoot time /1.5", 90, function() { ammo[2] /= 1.5 }), // *7
-    new Upgrade("Shoot time /1.5", 630, function() { ammo[2] /= 1.5 }),
-    new Upgrade("Shoot time /1.25", 4410, function() { ammo[2] /= 1.25 }),
+    new Upgrade("Shoot time /1.5", 90, function() { shoot[3] /= 1.5 }), // *7
+    new Upgrade("Shoot time /1.5", 630, function() { shoot[3] /= 1.5 }),
+    new Upgrade("Shoot time /1.25", 4410, function() { shoot[3] /= 1.25 }),
 
-    new Upgrade("Ammo stock x3", 90, function() { ammo[4] *= 3 }), // *15
-    new Upgrade("Ammo stock x3", 1350, function() { ammo[4] *= 3 }),
+    new Upgrade("Ammo stock x3", 90, function() { shoot[2] *= 3 }), // *15
+    new Upgrade("Ammo stock x3", 1350, function() { shoot[2] *= 3 }),
 
-    new Upgrade("Reload time /1.5", 270, function() { ammo[3] /= 1.5 }), // *7
-    new Upgrade("Reload time /1.5", 1890, function() { ammo[3] /= 1.5 }),
-    new Upgrade("Reload time /1.25", 13230, function() { ammo[3] /= 1.25 }),
+    new Upgrade("Reload time /1.5", 270, function() { shoot[4] /= 1.5 }), // *7
+    new Upgrade("Reload time /1.5", 1890, function() { shoot[4] /= 1.5 }),
+    new Upgrade("Reload time /1.25", 13230, function() { shoot[4] /= 1.25 }),
 
-    new Upgrade("Weed price x1.5", 600, function() {weed[1] *= 1.5 }), // *3
-    new Upgrade("Weed price x1.5", 1800, function() {weed[1] *= 1.5 }),
-    new Upgrade("Weed price x3", 5400, function() {weed[1] *= 3 }),
-    new Upgrade("Meth price x1.5", 1400, function() {meth[1] *= 1.5 }),
-    new Upgrade("Meth price x1.5", 4200, function() {meth[1] *= 1.5 }),
-    new Upgrade("Meth price x3", 12600, function() {meth[1] *= 3 })
+    new Upgrade("Weed price x1.5", 600, function() {dSoldPS[0] *= 1.5 }), // *3
+    new Upgrade("Weed price x1.5", 1800, function() {dSoldPS[0] *= 1.5 }),
+    new Upgrade("Weed price x3", 5400, function() {dSoldPS[0] *= 3 }),
+    new Upgrade("Meth price x1.5", 1400, function() {dSoldPS[1] *= 1.5 }),
+    new Upgrade("Meth price x1.5", 4200, function() {dSoldPS[1] *= 1.5 }),
+    new Upgrade("Meth price x3", 12600, function() {dSoldPS[1] *= 3 })
+];
+var helpersOwned;
+var helpers = [
+    new Helper("Shoot Helper", 325),
+    new Helper("Reload Helper", 950)
 ];
 var init; var fps = 60; var interval = (1000 / fps); var key = "BM-INC_";
 var allVars = ["money", "shoot", "prestige", "dStock", "dName", "dPrice", "dSoldPS", "upgradesOwned"];
@@ -60,6 +65,7 @@ function initGame() {
 
     for (var i = 0; i < upgrades.length; i++) {
         var u = upgrades[i];
+        $("#u-" + (i+1)).attr("onclick", "buyUpgrade(" + (i+1) + ");");
         $("#u-n" + (i+1)).html(u.name + " : ");
         $("#u-c" + (i+1)).html(fix(u.price, 2) + "$");
         if (upgradesOwned[i]) {
@@ -101,6 +107,11 @@ function displayGame() {
     $("#s-money").html("Money : " + fix(money[0], 2) + "$<br>");
     $("#s-totalmoney").html("Total money : " + fix(money[1], 2) + "$<br>");
     $("#h-ammo, #s-ammo").html("Ammo : " + shoot[0] + "/" + shoot[2]);
+
+    $("#a-n1").html("Shoot : +" + fix(shoot[1], 2) + "$");
+    $("#a-d1").html(fix(shoot[3]/1000, 1) + " sec/shoot");
+    $("#a-n2").html("Reload : +" + fix(shoot[2], 0) + " ammo");
+    $("#a-d2").html(fix(shoot[4]/1000, 1) + " sec");
 };
 
 // helpers functions
@@ -152,6 +163,19 @@ function Upgrade(name, price, run) {
     this.name = name;
     this.price = price;
     this.run = run;
+};
+function buyUpgrade(index) {
+    if (money[0] >= upgrades[index-1].price) {
+        money[0] -= upgrades[index-1].price;
+        upgradesOwned[index-1] = true;
+        upgrades[index-1].run();
+        $("#u-" + index).css("display", "none");
+        console.log(index);
+    };
+};
+function Helper(name, price) {
+    this.name = name;
+    this.price = price;
 };
 
 // loading + interval
