9b4dc5609d6a55298532c481d9010b0a2da55183
Tom <totom7700@gmail.com>
2015-01-19 20:20:49 +0100

    Blackmarket - Prototype v0.020r11

--- blackmarket/js/resize.js
+++ blackmarket/js/resize.js
@@ -1,3 +1,3 @@
-var maxpx = $(document).height(); var marginpx = 100;
+var maxpx = $(document).height(); var marginpx = 80;
 $(".container-fluid, .col-md-4").css('max-height', (maxpx - marginpx) + 'px');
 $(".col-md-4").css('overflow-y', 'auto');
