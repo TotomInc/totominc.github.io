a53ee31c6ab0f15f290ed3984005b6ad31308344
Tom <totom7700@gmail.com>
2015-01-27 17:48:44 +0100

    Blackmarket - Beta v0.1-fix7

--- blackmarket/js/resize.js
+++ blackmarket/js/resize.js
@@ -1,7 +1,7 @@
 var maxpx = $(document).height(); var marginpx = 80;
 $(".container-fluid, .col-md-4").css('max-height', (maxpx - marginpx) + 'px');
 $(".col-md-4").css('overflow-y', 'auto');
-if (maxpx < 580) {
+if (maxpx < 680) {
 	$(".navbar").css("display", "none");
 	$("body").css("margin", "10px 0 0 0");
 };
