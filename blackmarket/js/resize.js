456f60fd658dfc467236f0b0835539634b6e2e27
Tom <totom7700@gmail.com>
2015-02-03 14:34:30 +0100

    Revert "Blackmarket - Beta v0."
    This reverts commit 0e6f5ba8a04905cccb8bb34e59f3c8db53632a90.

--- blackmarket/js/resize.js
+++ blackmarket/js/resize.js
@@ -1,10 +1,3 @@
 var maxpx = $(document).height(); var marginpx = 120;
-var mobile = false;
 $(".container-fluid, .col-md-5, .col-md-7").css('max-height', (maxpx - marginpx) + 'px');
 $(".col-md-5, .col-md-7").css('overflow-y', 'auto');
-if (maxpx <= 900) {
-	mobile = true;
-	$("#s-d1, #s-d2, #s-d3").css("display", "true");
-} else {
-	$("#s-d1, #s-d2, #s-d3").css("display", "none");
-};
