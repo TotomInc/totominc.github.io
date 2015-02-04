e61878884447a453643f7ef32ff13d8cde240414
Tom <totom7700@gmail.com>
2015-02-04 20:56:11 +0100

    Blackmarket - Beta v0.13-fix6

--- blackmarket/js/resize.js
+++ blackmarket/js/resize.js
@@ -1,8 +1,11 @@
-var maxpx = $(document).height(); var marginpx = 120;
+var maxpx = $(document).height();
+var widthpx = $(document).width();
+var marginpx = 120;
 var mobile = false;
+
 $(".container-fluid, .col-md-5, .col-md-7").css('max-height', (maxpx - marginpx) + 'px');
 $(".col-md-5, .col-md-7").css('overflow-y', 'auto');
-if (maxpx <= 900) {
+if (widthpx <= 780) {
 	mobile = true;
 	$("#s-d1, #s-d2, #s-d3").css("display", "true");
 } else {
