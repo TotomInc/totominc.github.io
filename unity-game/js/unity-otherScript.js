0f88d6d3039231f980b03a68e6e0c6bec97a3e70
Tom <totom7700@gmail.com>
2015-02-20 16:11:39 +0100

    Unity Incremental

---
+++ unity-game/js/unity-otherScript.js
@@ -0,0 +1,1 @@
+var unityObjectUrl="http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js";"https:"==document.location.protocol&&(unityObjectUrl=unityObjectUrl.replace("http://","https://ssl-")),document.write('<script type="text/javascript" src="'+unityObjectUrl+'"></script>');
