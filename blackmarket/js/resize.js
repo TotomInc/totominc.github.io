5dcdd77538d67afa0cea21004f0f053f4880b561
Tom <totom7700@gmail.com>
2015-02-23 12:40:14 +0100

    Blackmarket - landscape + Incremental-RPG

--- blackmarket/js/resize.js
+++ blackmarket/js/resize.js
@@ -7,6 +7,7 @@ $(".container-fluid, .col-md-5, .col-md-7").css('max-height', (maxpx - marginpx)
 $(".col-md-5, .col-md-7").css('overflow-y', 'auto');
 if (widthpx <= 780) {
 	mobile = true;
+	window.alert("Please, play in landscape mode for a better compatibility! Thanks! :)")
 	$("#s-d1, #s-d2, #s-d3").css("display", "true");
 } else {
 	$("#s-d1, #s-d2, #s-d3").css("display", "none");
---
+++ inc-rpg/js/core.js
@@ -0,0 +1,7 @@
+var player = {
+	stats: { hp: 0, xp: 0, xpToLevelUp: 0, level: 1, goldCoin: 0, diamond: 0 },
+	helmet: { itemName: null, armor: null },
+	chestplate: { itemName: null, armor: null },
+	pants: { itemName: null, armor: null },
+	boots: { itemName: null, armor: null }
+}
