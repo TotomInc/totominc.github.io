e075a2222193d2617bc6b148a82d6a8e649d882e
Tom <totom7700@gmail.com>
2015-04-20 21:43:46 +0200

    Blackmarket - browser restrictions

---
+++ blackmarket/src/browser.js
@@ -0,0 +1,11 @@
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
+if (isIE == true) {
+	alert("Warning! You are using Internet Explorer, Blackmarket will not work with any of Internet Explorer versions, I recommend using Google Chrome.");
+};
