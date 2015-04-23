19602c1f8919526357927aac8d5faf2819197470
TotomInc <totom7700@gmail.com>
2015-04-23 13:53:02 +0200

    Blackmarket - scroll test (kongregate)

--- blackmarket/js/resize.js
+++ blackmarket/js/resize.js
@@ -8,8 +8,10 @@ function resize() {
 	if (widthpx >= 768) {
 		$("#panels-col").css('max-height', (maxpx - marginpx + panelspx) + 'px');
 		$("#panels-col").css('overflow-y', 'auto');
+		$("#panels-col").css('overflow', 'auto');
 		$(".tab-content").css('max-height', (maxpx - marginpx) + 'px');
 		$(".tab-content").css('overflow-y', 'auto');
+		$(".tab-content").css('overflow', 'auto');
 	} else {
 		$(".tab-content").css('max-height', (maxpx - marginpx) + 'px');
 		$("#panels-col").css('max-height', (maxpx - marginpx + panelspx) + 'px');
