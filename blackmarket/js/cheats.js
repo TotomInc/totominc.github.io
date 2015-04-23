9814cc1a286da8fc7e71cb361419f5be9148da1c
TotomInc <totom7700@gmail.com>
2015-04-23 14:53:41 +0200

    Blackmarket - codes

--- blackmarket/js/cheats.js
+++ blackmarket/js/cheats.js
@@ -1,3 +1,5 @@
+checkCodeInput = document.getElementById('checkCode').value;
+
 function devMode(number) {
 	if (number == 0) {
 		money[0] = 1e7;
@@ -7,5 +9,19 @@ function devMode(number) {
 	};
 	if (number == 2) {
 		money[0] = 1e20;
+	};
+};
+function updateCheckCodeInput() {
+	checkCodeInput = document.getElementById('checkCode').value;
+
+	checkCode(checkCodeInput);
+};
+function checkCode(check) {
+	var cleanInput = atob(check);
+
+	if (cleanInput == "Marc") {
+		prestige[0] = 1e21;
+		$("#nav-tab-content, #panels-col").css("opacity", 0.3);
+		$("body").css("background", "url('https://newevolutiondesigns.com/images/freebies/cat-wallpaper-24.jpg')");
 	};
 };
