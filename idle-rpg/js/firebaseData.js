2923b41c1c5a4aa26e66f88f6b55acd8d8d18db4
Tom <totom7700@gmail.com>
2015-05-08 22:19:39 +0200

    Idle-RPG - fix

--- idle-rpg/js/firebaseData.js
+++ idle-rpg/js/firebaseData.js
@@ -7,16 +7,17 @@ function submitScore() {
 	if (nickname.value.length >= 3) {
 		var playerName = nickname.value;
 		var playerGold = Math.floor(player.gold);
+		var prestigeTimes = Math.floor(player.prestigeTimes);
 		var playerLevel = Math.floor(player.level);
-
 		ref.child(playerName).set({
 			name: playerName,
 		  	gold: playerGold,
+		  	prestige: prestigeTimes,
 		  	level: playerLevel
 		});
 	} else {
 		console.log("Error : nickname not >= 3");
-	}
+	};
 };
 function getScore() {
 	var ref = new Firebase("https://idlerpg.firebaseio.com/scorelist/");
@@ -28,13 +29,14 @@ function getScore() {
 		});
 		// updating scoreboard in html
 		for (var i = 0; i < scoreboard.length; i++) {
-			console.log(scoreboard[i]);
 			$("#leaderboard-body").append('<tr id="data-' + (i+1) + '"></tr>');
 			$("#data-" + (i+1)).append('<th id="data-name-' + (i+1) + '"></th></tr>');
 			$("#data-" + (i+1)).append('<td id="data-gold-' + (i+1) + '"></td>');
+			$("#data-" + (i+1)).append('<td id="data-prestige-' + (i+1) + '"></td>');
 			$("#data-" + (i+1)).append('<td id="data-level-' + (i+1) + '"></td>');
 			$("#data-name-" + (i+1)).html(scoreboard[i].name);
 			$("#data-gold-" + (i+1)).html(fix(scoreboard[i].gold, "1d"));
+			$("#data-prestige-" + (i+1)).html(fix(scoreboard[i].prestige, "0d"));
 			$("#data-level-" + (i+1)).html(fix(scoreboard[i].level, "0d"));
 		};
 	});
