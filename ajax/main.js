79974c9e4503719c1a473001270206db797dbfb9
Tom <totom7700@gmail.com>
2015-07-21 20:40:25 +0200

    Removing AJAX test

--- ajax/main.js
+++
@@ -1,8 +0,0 @@
-var websites = ["http://totominc.github.io/ajax/blackmarket/"];
-
-var ajax = {};
-
-ajax.loadPage = function(index) {
-	$(".result").empty();
-	$(".result").load(websites[index]);
-};
